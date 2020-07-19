83531b391893805ceab6a2c253254da45ff6e680
puneethsaikumar <puneethsaikumar2000@gmail.com>
2020-07-19 19:27:36 +0530

    Created the solver.py the input is hardcoded

---
+++ sudoku.py
@@ -0,0 +1,83 @@
+board = [
+	[7,8,0,4,0,0,1,2,0],
+	[6,0,0,0,7,5,0,0,9],
+	[0,0,0,6,0,1,0,7,8],
+	[0,0,7,0,4,0,2,6,0],
+	[0,0,1,0,5,0,9,3,0],
+	[9,0,4,0,6,0,0,0,5],
+	[0,7,0,3,0,0,0,1,2],
+	[1,2,0,0,0,7,4,0,0],
+	[0,4,9,2,0,6,0,0,7]
+]
+
+def valid(bo, num, pos):
+	# check it's row
+	for i in range(len(bo[0])):
+		if bo[pos[0]][i] == num and pos[1] != i:
+			return False
+	
+	# check it's col
+	for i in range(len(bo)):
+		if bo[i][pos[1]] == num and pos[0] != i:
+			return False
+	
+	# check box
+	r = pos[0] // 3
+	c = pos[1] // 3
+	for i in range(3*r, 3*r + 3):
+		for j in range(3*c, 3*c + 3):
+			if (bo[i][j] == num and pos != (i, j)):
+				return False
+
+	return True
+
+
+def print_board(bo):
+	
+	for i in range(len(bo)):
+		if (i%3 == 0 and i != 0):
+			print("- - - - - - - - - - - -")
+
+		for j in range(len(bo[0])):
+			if (j%3 == 0 and j != 0):
+				print(" | ", end = "")
+
+			if (j == 8):
+				print(bo[i][j])
+			else:
+				print(str(bo[i][j]) + " ", end = "")
+
+# print_board(board)
+
+def find_empty(bo):
+	for i in range(len(bo)):
+		for j in range(len(bo[0])):
+			if (bo[i][j] == 0):
+				return (i, j) # row, col
+
+	return None
+
+
+def solve(bo):
+	find = find_empty(bo)
+	if not find:
+		return True
+	else:
+		r, c = find
+
+	for i in range(1,10):
+		if valid(bo, i, (r, c)):
+			bo[r][c] = i
+
+			if solve(bo):
+				return True
+
+		bo[r][c] = 0
+
+	return False
+
+
+print_board(board)
+solve(board)
+print("--------------------------------")
+print_board(board)
